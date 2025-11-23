86486ee21b3160bf71f2c1a7b83a0bd783a4f7f0
Manas Chimpidi <manaschimpidi@gmail.com>
2025-11-23 14:22:00 -0600

    fix navigation render

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { AppProvider } from "@/app/context/AppContext";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
+import { Navigation } from "@/components/Navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <body className={inter.className}>
-                <AppProvider>{children}</AppProvider>
+                <AppProvider>
+                    <Navigation />
+                    {children}
+                </AppProvider>
             </body>
         </html>
     );
