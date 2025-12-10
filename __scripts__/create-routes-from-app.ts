407e885a7b3c98769fc4532796a5eeae2b83e90a
Manas Chimpidi <manaschimpidi@gmail.com>
2025-12-09 20:34:31 -0600

    checkpoint

--- __scripts__/create-routes-from-app.ts
+++ __scripts__/create-routes-from-app.ts
@@ -1,3 +1,7 @@
+import fs from "fs";
+import path from "path";
+import jscodeshift from "jscodeshift";
+
 const basicRoutes = {
     login: "LoginForm",
     studios: "PublicStudiosDirectory",
@@ -22,6 +26,76 @@ const basicRoutes = {
     mudlyai: "MudlyAI"
 };
 
+const j = jscodeshift.withParser("tsx");
+
+for (const [route, component] of Object.entries(basicRoutes)) {
+    const componentFile = path.join("src", "components", component + ".tsx");
+    const routeFile = path.join("src", "app", route, "page.tsx");
+
+    updateImports(componentFile);
+    // moveFile(component, route, componentFile, routeFile);
+}
+
+/**
+ *
+ * "./ui/...." to "@/components/ui/..."
+ */
+function updateImports(componentFile: string) {
+    const updatedContent = j(fs.readFileSync(componentFile, "utf-8"))
+        .find(j.ImportDeclaration, {
+            source: {
+                value: (value: string) => value.startsWith(".")
+            }
+        })
+        .forEach((p) => {
+            const oldImport = p.node.source.value;
+            const newImport =
+            console.log(`updating import in ${componentFile}: ${p.node.source.value}`);
+        })
+        .toSource();
+
+    // fs.writeFileSync(componentFile, updatedContent, "utf-8");
+}
+
+function moveFile(
+    componentName: string,
+    routeName: string,
+    componentFile: string,
+    routeFile: string
+) {
+    console.log(`moving ${componentName} to route: /${routeName}/page.tsx`);
+
+    if (!fs.existsSync(componentFile)) {
+        console.warn(`Component file does not exist: ${componentFile}`);
+        return;
+    }
+
+    // create route, make all subdirs
+    fs.mkdirSync(path.dirname(routeFile), { recursive: true });
+
+    // update export to be default
+    const updatedContent = j(fs.readFileSync(componentFile, "utf-8"))
+        .find(j.ExportNamedDeclaration, {
+            declaration: {
+                id: {
+                    name: componentName
+                }
+            }
+        })
+        .forEach((p) => {
+            if (
+                p.node.declaration?.type === "FunctionDeclaration" &&
+                p.node.declaration.id?.name === componentName
+            ) {
+                p.replace(j.exportDefaultDeclaration(p.node.declaration));
+            }
+        })
+        .toSource();
+
+    // move file
+    fs.renameSync(componentFile, routeFile);
+    fs.writeFileSync(routeFile, updatedContent, "utf-8");
+}
 // Special Cases:
 // not loggedin
 // return (
